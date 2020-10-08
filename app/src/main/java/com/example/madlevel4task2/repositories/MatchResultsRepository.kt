cb0591edca29dd5e65a205a254aa89c992141408
Sven van Putten <svenvanputten@gmail.com>
2020-10-08 23:20:35 +0200

    Added comments to the MatchResultsRepository

--- app/src/main/java/com/example/madlevel4task2/repositories/MatchResultsRepository.kt
+++ app/src/main/java/com/example/madlevel4task2/repositories/MatchResultsRepository.kt
@@ -2,7 +2,6 @@ package com.example.madlevel4task2.repositories
 
 import android.content.Context
 import com.example.madlevel4task2.database.GameResultListRoomDatabase
-import com.example.madlevel4task2.entities.MatchOption
 import com.example.madlevel4task2.entities.MatchOutcome
 import com.example.madlevel4task2.entities.MatchResult
 import com.example.madlevel4task2.interfaces.MatchResultsDao
@@ -11,35 +10,60 @@ class MatchResultsRepository(context: Context) {
 
     private val matchResultsDao: MatchResultsDao
 
+    /**
+     * Initialise this repository with an instance of the GameResultListRoomDatabase
+     * and a MatchResults Data Access Object
+     */
     init {
         val database = GameResultListRoomDatabase.getDatabase(context)
         matchResultsDao = database!!.matchResultsDao()
     }
 
+    /**
+     * Get and return a list of all played games.
+     */
     suspend fun getAllMatchResults(): List<MatchResult> {
         return matchResultsDao.getAllResults()
     }
 
+    /**
+     * Insert a new [MatchResult] object into the database.
+     */
     suspend fun insertMatchResult(matchResult: MatchResult) {
         matchResultsDao.insertResult(matchResult)
     }
 
+    /**
+     * Delete a [MatchResult] from the database.
+     */
     suspend fun deleteMatchResult(matchResult: MatchResult) {
         matchResultsDao.deleteResult(matchResult)
     }
 
+    /**
+     * Remove all data of played games.
+     */
     suspend fun deleteAllMatchResults() {
         matchResultsDao.deleteAllMatchResults()
     }
 
+    /**
+     * Get the amount of wins from played games.
+     */
     suspend fun getNumberOfWins(): Int {
         return matchResultsDao.getNumberOfWins(MatchOutcome.WIN)
     }
 
+    /**
+     * Get the amount of draws from played games.
+     */
     suspend fun getNumberOfDraws(): Int {
         return matchResultsDao.getNumberOfWins(MatchOutcome.DRAW)
     }
 
+    /**
+     * Get the amount of losses from played games.
+     */
     suspend fun getNumberOLosses(): Int {
         return matchResultsDao.getNumberOfWins(MatchOutcome.LOSS)
     }
