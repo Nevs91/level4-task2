f59a1eaf1ac269d3f768afbc4e39f13324adc7e9
Sven van Putten <svenvanputten@gmail.com>
2020-10-08 18:16:15 +0200

    Renamed the fragment classes and layouts, moved the classes to the ui package

--- app/src/main/java/com/example/madlevel4task2/ui/GameFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/ui/GameFragment.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel4task2
+package com.example.madlevel4task2.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,18 +7,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.madlevel4task2.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class GameFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
