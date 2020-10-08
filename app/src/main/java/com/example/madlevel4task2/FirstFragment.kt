1ee04f48de00ea7b2504308c6819a8c87522b466
Sven van Putten <svenvanputten@gmail.com>
2020-10-08 17:53:32 +0200

    Revert "Renamed fragments and their layout files and moved them to a new 'ui' package"
    This reverts commit 8fa406f3

--- app/src/main/java/com/example/madlevel4task2/FirstFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/FirstFragment.kt
@@ -1,4 +1,4 @@
-package com.example.madlevel4task2.ui
+package com.example.madlevel4task2
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,19 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
-import com.example.madlevel4task2.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class GameFragment : Fragment() {
+class FirstFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
