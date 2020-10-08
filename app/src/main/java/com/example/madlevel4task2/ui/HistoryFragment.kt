d55c33a0f6988e27a364fb764d5f80d5314bd284
Sven van Putten <svenvanputten@gmail.com>
2020-10-08 19:47:18 +0200

    Created a new menu for the history fragment, added click events for navigating between fragments.

--- app/src/main/java/com/example/madlevel4task2/ui/HistoryFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/ui/HistoryFragment.kt
@@ -1,12 +1,8 @@
 package com.example.madlevel4task2.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.madlevel4task2.R
 
 /**
@@ -24,9 +20,24 @@ class HistoryFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_history_fragment, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_clear_history -> {
+                clearResultsHistory()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun clearResultsHistory() {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
+++ app/src/main/java/com/example/madlevel4task2/ui/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.madlevel4task2.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        navController = findNavController(R.id.nav_host_fragment)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -20,12 +26,18 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
+    /**
+     * Handle action bar item clicks here.
+     *
+     * The action bar will automatically handle clicks on the Home/Up button,
+     * so long as you specify a parent activity in AndroidManifest.xml.
+     */
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_history -> {
+                navController.navigate(R.id.action_GameFragment_to_HistoryFragment)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
